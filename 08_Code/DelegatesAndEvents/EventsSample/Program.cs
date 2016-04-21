53f82f28d37c77821062ab5d8b2b429eaa7aa31c
Ricky-Hao <a471558277@gmail.com>
2016-04-21 09:53:23 +0800

    Add Code

---
+++ 08_Code/DelegatesAndEvents/EventsSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Wrox.ProCSharp.Delegates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dealer = new CarDealer();
+
+            var michael = new Consumer("Michael");
+            dealer.NewCarInfo += michael.NewCarIsHere;
+
+            dealer.NewCar("Ferrari");
+
+            var nick = new Consumer("Sebastian");
+            dealer.NewCarInfo += nick.NewCarIsHere;
+
+            dealer.NewCar("Mercedes");
+
+            dealer.NewCarInfo -= michael.NewCarIsHere;
+
+            dealer.NewCar("Red Bull Racing");
+
+            Console.ReadKey();
+        }
+    }
+}
